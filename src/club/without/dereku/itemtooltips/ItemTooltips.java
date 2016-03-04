3de0981609a9e22ff63d7a0bb6ee2d12faf3f741
Dereku <dereku@default.im>
2016-03-04 11:13:27 +0700

    Bump

--- src/club/without/dereku/itemtooltips/ItemTooltips.java
+++ src/club/without/dereku/itemtooltips/ItemTooltips.java
@@ -50,7 +50,6 @@ public class ItemTooltips extends JavaPlugin {
     public void onEnable() {
         this.saveDefaultConfig();
         this.language = this.getConfig().getString("lang", "en_US");
-        
         if (this.getServer().getBukkitVersion().startsWith("1.8.8")) {
             this.impl = new v1_8_R3();
         } else if (this.getServer().getBukkitVersion().startsWith("1.9-R0.1")) {
